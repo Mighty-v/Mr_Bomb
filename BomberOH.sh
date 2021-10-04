ac47694017fde47c309a4f617e67a806f499de6d
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-10-04 10:33:13 +0530

    Update BomberOH.sh

--- BomberOH.sh
+++ BomberOH.sh
@@ -51,10 +51,19 @@ echo -e "\e[95m
      █▀█ █▄░█ █░░ █ █▄░█ █▀▀   █░█ ▄▀█ █▀▀ █▄▀ █ █▄░█ █▀▀
      █▄█ █░▀█ █▄▄ █ █░▀█ ██▄   █▀█ █▀█ █▄▄ █░█ █ █░▀█ █▄█ \e[95;1m \e[0m\n"
 echo""
-sleep 1
-echo "                                    $WHITE Author:$RED Anubhav Kashyap"
 echo ""
-
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m FREE FIRE PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.5] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github :- \e[36mhttps://github.com/OnlineHacKing/Mr_Bomb \e[0m\e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    |O|n|l|i|n|e| |H|a|c|k|i|n|g|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m' 
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[95m    |S|U|M|A|N|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m' 
+      echo ""                                               
+echo ""
 }
 
 #    Check Internet
