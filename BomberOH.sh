9f8010a3bfd27b554e4a55430241d05b96d78b55
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-10-04 10:42:08 +0530

    Update BomberOH.sh

--- BomberOH.sh
+++ BomberOH.sh
@@ -51,16 +51,13 @@ echo -e "\e[95m
      █▄█ █░▀█ █▄▄ █ █░▀█ ██▄   █▀█ █▀█ █▄▄ █░█ █ █░▀█ █▄█ \e[95;1m \e[0m\n"
 echo""
 echo ""
-  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m FREE FIRE PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.5] \e[0m'                                       
-  echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github :- \e[36mhttps://github.com/OnlineHacKing/Mr_Bomb \e[0m\e[1;32m\e[0m\e[1;33m] \e[0m'
-   echo ""
+ echo -e $'\e[1;33m\e[0m\e[1;33m██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m Super SMS & Call Bomber \e[0m'                                  
+ echo ""
+ echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/OnlineHacKing/Mr_Bomb \e[0m\e[1;32m\e[0m\e[1;33m] \e[0m'
+ echo ""
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
       echo -e "\e[93m    |O|n|l|i|n|e| |H|a|c|k|i|n|g|"      
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m' 
-      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m'
-      echo -e "\e[95m    |S|U|M|A|N|"      
-      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m' 
       echo ""                                               
 echo ""
 }
